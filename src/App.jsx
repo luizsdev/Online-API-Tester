1c176edbec861566fcde122c51ca8f0e0642c30b
Luiz Sanches <luizsdev@gmail.com>
2022-06-09 18:10:42 -0300

    initial design

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 id="title">API TESTER</h1>
+      <div className="container">
+        <button>GET</button>
+        <button>POST</button>
+        <button>PUT</button>
+        <button>DELETE</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
