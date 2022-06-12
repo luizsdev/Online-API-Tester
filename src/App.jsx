8d52496d3133fbb569c8400b26c95ac7a5e3d80b
Luiz Sanches <luizsdev@gmail.com>
2022-06-12 01:06:48 -0300

    showing requests

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
 import "./App.css";
-
+import Select from "react-select";
+import { useState } from "react";
+import axios from "axios";
 function App() {
+  const [ressa, setRessa] = useState("");
+  const [url, setUrl] = useState("");
+  var method = "";
+  const options = [
+    { value: "GET", label: "GET" },
+    { value: "POST", label: "POST" },
+  ];
+  function handleClick(e) {
+    method = e.value;
+  }
+  function handleInput(e) {
+    setUrl(e.target.value);
+  }
+  async function handleButton(e) {
+    if (method == "GET") {
+      await axios
+        .get(url)
+        .then((res) => {
+          setRessa(res);
+          console.log(res);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      console.log("Couldn't request");
+    }
+  }
+
   return (
     <div className="App">
       <h1 id="title">API TESTER</h1>
       <div className="container">
-        <button>GET</button>
-        <button>POST</button>
-        <button>PUT</button>
-        <button>DELETE</button>
+        <Select
+          onChange={handleClick}
+          id="selecting"
+          options={options}
+        ></Select>
+        <input onChange={handleInput} type="text" placeholder="URL" id="url" />
+        <button onClick={handleButton} type="submit" id="submit">
+          REQUEST
+        </button>
       </div>
     </div>
   );
